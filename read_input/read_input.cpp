d8a9a4e4453ff1e9b14db333f9f2fe9ea156d8c5
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2018-05-03 17:17:44 -0500

    updated 5/3 5.17pm

--- read_input/read_input.cpp
+++ read_input/read_input.cpp
@@ -9,37 +9,48 @@
 #include <sstream>
 #include <iterator>
 using namespace std;
-// Driver program
-int main() {
 
-    int n = 0, i = 0;
-    string line;
+void read_from_input(int choice_of_file, int choice_of_distribution, float array[], int n, int k, string timestamp) {
+
+    int i = 0;
     ifstream inFile;
+    string line, num;
+    string file = "C:\\Users\\kavya\\CLionProjects\\input_data\\File";
 
-    // reading input file to get k, n, array[]:
-    int k = 0;
-    string num;
+    if(choice_of_distribution == 1)
+        file.append(choice_of_file+"_Uniform_"+timestamp+".txt");
+    else
+        file.append(choice_of_file+"_Normal_"+timestamp+".txt");
 
     // for input including k, n and array:
-    inFile.open("C:\\Users\\kavya\\CLionProjects\\input_data\\Book1.csv");
+    inFile.open(file);
 
     if (!inFile) {
         cout << "Unable to open file";
         exit(1); // terminate with error
     }
 
-    while( getline(inFile, line) )
-    {
-        if(i == 0)
-            num = line;
-        break;
+    if(choice_of_file == 1){
+        while( getline(inFile, line) )
+        {
+            if(i == 0)
+                num = line;
+            break;
+        }
+        std::istringstream buf(num);
+        std::istream_iterator<std::string> beg(buf), end;
+        std::vector<std::string> tokens(beg, end);
+        k = stoi(tokens[0]);
+        n = stoi(tokens[1]);
     }
-    std::istringstream buf(num);
-    std::istream_iterator<std::string> beg(buf), end;
-    std::vector<std::string> tokens(beg, end);
-    k = stoi(tokens[0]);
-    n = stoi(tokens[1]);
-    float array[n];
+    else{
+        while( getline(inFile, line) )
+        {
+            if(i == 0)
+                n = stoi(line);
+            break;
+        }
+    }
     i = 1;
     while( getline(inFile, line) ) {
         array[i-1] = stof(line);
@@ -47,39 +58,4 @@ int main() {
     }
     cout << "\nk:"<<k<<"\n";
     cout << "\nn:"<<n<<"\n";
-
- /* Uncomment for
-  * // reading input file to get n, array[]:
-
-    inFile.open("C:\\Users\\kavya\\CLionProjects\\input_data\\Book2.csv");
-    if (!inFile) {
-        cout << "Unable to open file";
-        exit(1); // terminate with error
-    }
-    string line2;
-    i = 0;
-    while( getline(inFile, line) )
-    {
-        if(i == 0)
-            n = stoi(line);
-        break;
-    }
-    float array[n];
-    i = 1;
-    while(getline(inFile, line)) {
-        array[i-1] = stof(line);
-        i += 1;
-    }
-    cout << "\n n:"<<n<<"\n";
-
-    inFile.close();
-
-*/
-
-    // print array
-    for(int j = 0; j < n ; j++) {
-        cout <<"\narray["<<j<<"] : "<<array[j];
-    }
-    return 0;
-
 }
