ea59806e22fe8b5762ebf17b852e3a83b060c253
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2018-05-03 17:21:20 -0500

    updated 5/3 5.21pm

--- read_input/generate_input.cpp
+++ read_input/generate_input.cpp
@@ -8,38 +8,12 @@
 #include <fstream>
 using namespace std;
 
-void generate_input(int n, int k, int choice_of_file, int choice_of_distribution) {
-	/* 	n -> number of elements in the array
-	 * choice_of_file = 1 -> input file with k
-	 * choice_of_file = 2 -> input file without k
-	 * choice_of_distribution = 1 -> uniform distribution
-	 * choice_of_distribution = 2 -> normal distribution
-	 */
-	if((choice_of_file == 1 or choice_of_file == 2) and (choice_of_distribution == 1 or choice_of_distribution == 2))
-	{
-		switch(choice_of_file) {
-			case 1:
-				if (choice_of_distribution == 1)
-					generate_file1_uniform(n, k);
-				else
-					generate_file1_normal(n, k);
-				break;
-			case 2:
-				if (choice_of_distribution == 2)
-					generate_file2_uniform(n);
-				else
-					generate_file2_normal(n);
-				break;
-		}
-	}
-	else
-		cout<<"Invalid input! choice_of_file and choice_of_distribution must be either 1 or 2 !";
-}
+string get_date(void);
 
-void generate_file1_uniform(int n, int k){
-
+string generate_file1_uniform(int n, int k){
 	ofstream file1;  // Create Object of Ofstream
-	file1.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File1_Uniform.txt"); // Opening a File or creating if not present
+    string timestamp = get_date();
+	file1.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File1_Uniform_"+timestamp+".txt"); // Opening a File or creating if not present
 	file1 << n <<" "<< k; // Writing data to file
 	cout<<"Data has been written to file";
 
@@ -52,11 +26,13 @@ void generate_file1_uniform(int n, int k){
     }
     cout << "\nUniform Data has been written to file with n, k and n elements!";
     file1.close();
+    return timestamp;
 }
 
-void generate_file2_uniform(int n) {
+string generate_file2_uniform(int n) {
     ofstream file2;  // Create Object of Ofstream
-    file2.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File2_Uniform.txt"); // Opening a File or creating if not present
+    string timestamp = get_date();
+    file2.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File2_Uniform_"+timestamp+".txt"); // Opening a File or creating if not present
     file2 << n; // Writing data to file
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -67,12 +43,14 @@ void generate_file2_uniform(int n) {
     }
     cout << "\nUniform Data has been written to file2 with n and n elements!";
     file2.close();
+    return timestamp;
 }
 
-void generate_file1_normal(int n, int k){
+string generate_file1_normal(int n, int k){
 
     ofstream file1;  // Create Object of Ofstream
-    file1.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File1_Normal.txt"); // Opening a File or creating if not present
+    string timestamp = get_date();
+    file1.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File1_Normal_"+timestamp+".txt"); // Opening a File or creating if not present
     file1 << n <<" "<< k; // Writing data to file
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -84,11 +62,13 @@ void generate_file1_normal(int n, int k){
     }
     cout << "\nNormal Data has been written to file with n, k and n elements!";
     file1.close();
+    return timestamp;
 }
 
-void generate_file2_normal(int n) {
+string generate_file2_normal(int n) {
     ofstream file2;  // Create Object of Ofstream
-    file2.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File2_Normal.txt"); // Opening a File or creating if not present
+    string timestamp = get_date();
+    file2.open("C:\\Users\\kavya\\CLionProjects\\input_data\\File2_Normal_"+timestamp+".txt"); // Opening a File or creating if not present
     file2 << n; // Writing data to file
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -100,5 +80,45 @@ void generate_file2_normal(int n) {
     }
     cout << "\nNormal Data has been written to file2 with n and n elements!";
     file2.close();
+    return timestamp;
 }
 
+string get_date(void)
+{
+    time_t now;
+    char the_date[20];
+    the_date[0] = '\0';
+    now = time(NULL);
+    if (now != -1)
+        strftime(the_date, 20, "%d_%m_%Y_%H_%M_%S", gmtime(&now));
+    return std::string(the_date);
+}
+
+string generate_input(int n, int k, int choice_of_file, int choice_of_distribution) {
+    /* 	n -> number of elements in the array
+     * choice_of_file = 1 -> input file with k
+     * choice_of_file = 2 -> input file without k
+     * choice_of_distribution = 1 -> uniform distribution
+     * choice_of_distribution = 2 -> normal distribution
+     */
+    string timestamp;
+    if((choice_of_file == 1 or choice_of_file == 2) and (choice_of_distribution == 1 or choice_of_distribution == 2))
+    {
+        switch(choice_of_file) {
+            case 1:
+                if (choice_of_distribution == 1)
+                    timestamp = generate_file1_uniform(n, k);
+                else
+                    timestamp = generate_file1_normal(n, k);
+                break;
+            case 2:
+                if (choice_of_distribution == 2)
+                    timestamp = generate_file2_uniform(n);
+                else
+                    timestamp = generate_file2_normal(n);
+                break;
+        }
+    }
+    else
+        cout<<"Invalid input! choice_of_file and choice_of_distribution must be either 1 or 2 !";
+}
