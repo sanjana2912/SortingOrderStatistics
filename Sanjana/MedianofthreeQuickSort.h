4fe9e910fa0efe491e6ecc52c1f22b894e897a9b
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2018-05-04 20:42:55 -0500

    updated methods and argument types

--- Sanjana/MedianofthreeQuickSort.h
+++ Sanjana/MedianofthreeQuickSort.h
@@ -4,17 +4,26 @@
 
 #ifndef UNTITLED_MEDIANOFTHREEQUICKSORT_H
 #define UNTITLED_MEDIANOFTHREEQUICKSORT_H
-#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <chrono>
+#include "GenerateInput.h"
+using namespace std;
+using get_time = chrono::steady_clock ;
 using namespace std;
 
 class MedianofthreeQuickSort {
-    public:
-    static int calc_median(int *arr);
-    static int partition_median(int arr[], int low, int high);
-
+public:
+    static float calc_median(float arr[], int n);
+    static void print_Array_median(float arr[], int size);
+    static int partition_median(float arr[], int low, int high);
+    static void quickSort_median(float arr[], int low, int high);
+    static double quickSortmedian(float *arr, int low, int high);
 };
 
 
