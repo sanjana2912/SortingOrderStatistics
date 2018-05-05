30ee229b5da061388f9b89ded2c775e9246b0c58
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2018-05-04 20:45:05 -0500

    updated final header

--- Kavya/top_k_smallest_elements.h
+++ Kavya/top_k_smallest_elements.h
@@ -1,13 +1,26 @@
 //
-// Created by kavya on 4/22/2018.
+// Created by kavya on 5/3/2018.
 //
 
-#ifndef UNTITLED_TOP_K_SMALLEST_ELEMENTS_H
-#define UNTITLED_TOP_K_SMALLEST_ELEMENTS_H
+#ifndef MENU_TOPKSMALLEST_H
+#define MENU_TOPKSMALLEST_H
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <time.h>
+#include "GenerateInput.h"
 
-void swap(float array[],int i ,int j);
-void max_heapify(float array[], int i, int n);
-void print(float array[], int n);
-void top_k_smallest_elements(float array[], int n, int k);
+using namespace std;
+using get_time = chrono::steady_clock ;
 
-#endif //UNTITLED_TOP_K_SMALLEST_ELEMENTS_H
+class TopKSmallest {
+public:
+    static void swap(float array[],int i ,int j);
+    static void max_heapify(float array[], int i, int n);
+    static void print_and_write(float array[], int n);
+    static double top_k_smallest_elements(float array[], int n, int k);
+    static void write_runtime_to_file(int n, float runtime);
+};
+
+
+#endif //MENU_TOPKSMALLEST_H
